a2a90dcd364cdc75e0cc2b878fb4495e2a797e44
UP743982 <33805417+Lukas112@users.noreply.github.com>
2018-02-28 02:21:05 +0000

    scrubflix.2.6
    Implemented new functionality - genre options.
    
    Now user can filter displayed movies corresponding to the genres.
    
    Uploaded 12 movies for each genre only to show off functionality. More can be added using databases (leave it to Mark).
    
    Correccted some errors. Changed some design details.
    
    Created new files:
    script.js (responsible for genre option function).
    Created new stylesheet for genre page.
    Edited ReadMe file explaining which styesheet responsible for each page.
    
    THINGS TO DO (for the front end):
    1. Using bootstrap grid layout re-design front page movie layout (not as responsive as for now). Add different movie pictures.
    2. Create overlay effects on genre page. Add correct data.
    3.Create modal window for movies (front page, top20 page and genre page).
    4. Create login System.

--- README.md
+++ README.md
@@ -5,6 +5,9 @@
 # CSS folder consist of CSS Image Hover Effect Library available under MIT License: http://www.opensource.org/licenses/mit-license.php
 # Design folder consist of differnet design patterns to be implemented on the website. During implementation stage Design folder will include number of different design patterns, however in the final stage of the implementation only those used in the prototype will be left in the folder.
 # Image folder consist of movie posters used in the prototype. It will change consistently due to the nature of the software.
+# Style.css - index.html
+# Style2.css - top20.html
+# Style3.css - genre.html
 ******************************************************* TEAM ************************************************************
 # Software engineering team consist of 5 people.
 # Two people are working on the back-end of the application.
---
+++ script.js
@@ -0,0 +1,50 @@
+
+jQuery('document').ready( function($) {
+    $('.gallery-filters li').on('click', function() {
+        $('.gallery-filters li').removeClass('active');
+        $(this).addClass('active');
+        filter = $(this).attr('gallery-filter');
+
+        $('.gallery-img-holder').each( function() {
+            if (filter == 'all') {
+                $(this).fadeIn(); 
+            } else {
+                $(this).hide();
+                if ($(this).attr('filter-category') == filter) {
+                    $(this).fadeIn();
+                }
+            }
+        });
+    });
+});
+
+
+
+(function() {
+  var $imgs = $('.row gallery-images img');
+  var $search = $('#filter-search');
+  var cache = [];
+
+  $imgs.each(function() {
+    cache.push({
+      element: this,
+      text: this.alt.trim().toLowerCase()
+    })
+  })
+
+  function filter() {
+    var query = this.value.trim().toLowerCase();
+    cache.forEach(function(img) {
+      var index = 0;
+      if (query) {
+        index = img.text.indexOf(query);
+      }
+      img.element.style.display = index === -1 ? 'none' : '';
+    })
+  }
+  if ('oninput' in $search[0]) {
+    $search.on('input', filter);
+  } else {
+    $search.on('keyup', filter);
+  }
+}())
