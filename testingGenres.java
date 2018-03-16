cb0135a9d6d18fb85386cfb9c6e05555b93f0e8e
UP743982 <33805417+Lukas112@users.noreply.github.com>
2018-03-16 19:54:22 +0000

    Testing
    Testing genre page

---
+++ testingGenres.java
@@ -0,0 +1,13 @@
+import static com.heliumhq.API.*;
+
+public class public static void main(String[] args) {
+	start_chrome();
+	go_to("file:///C:/Users/User/Desktop/SCRUBFLIX%20VC/scrubflix.2.9%20-%20(current)/genres.html");
+	click("comedy");
+	click("Change Theme");
+	click("Green");
+	click("Change Theme");
+	click("Gold");
+	click("Change Theme");
+	click("Default");
+}
